212dd686cd9f4f07877ed20adf919f181dd2e46f
Leotrin Elmazi <leotrin.elmazi@gmail.com>
2019-06-05 12:34:36 +0200

    updated libraries

--- vendor/laravel/framework/src/Illuminate/Routing/Router.php
+++ vendor/laravel/framework/src/Illuminate/Routing/Router.php
@@ -1123,7 +1123,6 @@ class Router implements RegistrarContract, BindingRegistrar
         $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
         $this->post('login', 'Auth\LoginController@login');
         $this->post('logout', 'Auth\LoginController@logout')->name('logout');
-        $this->get('logout', 'Auth\LoginController@logout')->name('logout');
 
         // Registration Routes...
         $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
